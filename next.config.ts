d6516b4fc1f7b6e491254a3691a21ae0e1d2cf2f
Jati Suryo Buwono <jati.suryo@gmail.com>
2026-06-07 17:24:31 +0700

    build: update config

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
 
   images: {
     dangerouslyAllowLocalIP: true,
+    localPatterns: [
+      {
+        pathname: "/**",
+      },
+    ],
     remotePatterns: [
       {
         protocol: "http",
