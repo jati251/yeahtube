0f17385688cb2c5c7454fbb1fc4049935ba263ee
Jati Suryo Buwono <jati.suryo@gmail.com>
2026-07-05 19:56:43 +0700

    Remove test scripts

--- test-db.js
+++
@@ -1,5 +0,0 @@
-const { Pool } = require("pg");
-const pool = new Pool({ connectionString: "postgresql://postgres:PasswordKeramat123@192.168.1.41:5432/yeahtube" });
-pool.query("SELECT 1")
-  .then(() => { console.log("DB connection successful!"); process.exit(0); })
-  .catch((e) => { console.error("DB connection failed:", e); process.exit(1); });
